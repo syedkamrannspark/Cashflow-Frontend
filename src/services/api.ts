e1398d3fdc0eb447bff3272321abc64ab8a4c67c
kamran <syedkamran@sparkprescience.com>
2026-01-21 19:02:18 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'http://100.53.14.81:8000/api/v1';
 
 export const api = axios.create({
     baseURL: API_URL,
