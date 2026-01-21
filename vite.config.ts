3adb28f98c4037953921c5b8695d0d0788cf5078
kamran <syedkamran@sparkprescience.com>
2026-01-21 19:19:02 +0530

    vute config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [
@@ -14,6 +18,14 @@ export default defineConfig({
     alias: {
       // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://100.53.14.81:8000',
+        changeOrigin: true,
+      },
     },
   },
 })
